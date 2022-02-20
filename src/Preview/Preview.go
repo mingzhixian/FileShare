46e94cd1365e04f58f89e26cd4cd0f6feef377d3
kic <mingzhixian123@gmail.com>
2022-02-20 22:06:40 +0800

    增加视频音频文本文件图片的预览

--- src/Preview/Preview.go
+++ src/Preview/Preview.go
@@ -36,19 +36,24 @@ func Preview(response http.ResponseWriter, request *http.Request) {
 //检测文件类型，并解析
 func viewfile(File string) string {
 	prefix := path.Ext(File)[1:]
-	attachment = "<link href=\"./Static/css/" + prefix + ".css\" type=\"text/css\" rel=\"stylesheet\">"
-	if prefix == "txt" {
+	if prefix == "txt" || prefix == "css" || prefix == "js" || prefix == "html" || prefix == "py" || prefix == "java" || prefix == "go" || prefix == "c" || prefix == "cpp" || prefix == "cs" {
+		attachment = "<link href=\"./Static/css/txt.css\" type=\"text/css\" rel=\"stylesheet\">"
 		return viewTxt(File)
 	} else if prefix == "md" {
+		attachment = "<link href=\"./Static/css/md.css\" type=\"text/css\" rel=\"stylesheet\">"
 		return viewMarkdown(File)
-	} else if prefix == "mp4" || prefix == "webp" {
-		return viewMarkdown(File)
-	} else if prefix == "video" {
-		return view(File)
+	} else if prefix == "mp4" {
+		attachment = "<link href=\"./Static/css/video.css\" type=\"text/css\" rel=\"stylesheet\">"
+		return viewVideo(File)
+	} else if prefix == "mp3" {
+		attachment = "<link href=\"./Static/css/audio.css\" type=\"text/css\" rel=\"stylesheet\">"
+		return viewAudio(File)
 	} else if prefix == "audio" {
+		attachment = "<link href=\"./Static/css/txt.css\" type=\"text/css\" rel=\"stylesheet\">"
 		return viewMarkdown(File)
-	} else if prefix == "img" {
-		return viewMarkdown(File)
+	} else if prefix == "jpg" || prefix == "png" || prefix == "svg" || prefix == "webp" {
+		attachment = "<link href=\"./Static/css/img.css\" type=\"text/css\" rel=\"stylesheet\">"
+		return viewImg(File)
 	} else {
 		return "暂不支持此格式预览"
 	}
@@ -73,6 +78,14 @@ func viewMarkdown(File string) string {
 
 func viewVideo(File string) string {
 	return "<video id=\"video\" src=\"./Download?file=" + File + "\"></video>"
+}
+
+func viewAudio(File string) string {
+	return "<audio id=\"audio\" src=\"./Download?file=" + File + "\"></audio>"
+}
+
+func viewImg(File string) string {
+	return "<img id=\"img\" src=\"./Download?file=" + File + "\"></img>"
 }
 
 //组装并返回数据
